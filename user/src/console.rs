6d8a6b6995fdf5ccb227195199c310fe97d7c4a8
jpz <rengeyouzh1@163.com>
2023-02-08 19:14:08 +0800

    modify dir

--- user/src/console.rs
+++ user/src/console.rs
@@ -3,6 +3,9 @@
 use crate::write;
 use alloc::collections::vec_deque::VecDeque;
 use core::fmt::{self, Write};
+use lazy_static::*;
+use spin::mutex::Mutex;
+use alloc::sync::Arc;
 
 pub const STDIN: usize = 0;
 pub const STDOUT: usize = 1;
@@ -24,8 +27,37 @@ impl Write for ConsoleBuffer {
 impl ConsoleBuffer {
     fn drain_buffer(&mut self) -> isize {
         let str = self.0.make_contiguous();
-        let ret = write(STDOUT, str.as_ptr() as usize, str.len());
+        let ret = write(STDOUT, str);
         self.0.clear();
         ret
     }
+}
+lazy_static!{
+    static ref CONSOLE_BUFFER: Arc<Mutex<ConsoleBuffer>> = {
+        let buffer = VecDeque::<u8>::with_capacity(CONSOLE_BUFFER_SIZE);
+        Arc::new(Mutex::new(ConsoleBuffer(buffer)))
+    };
+}
+pub fn print(args: fmt::Arguments) {
+
+    let mut buf = CONSOLE_BUFFER.lock();
+    buf.write_fmt(args);
+}
+#[macro_export]
+macro_rules! print{
+    ($fmt: literal $(, $($args: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($args)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+
+pub fn flush() {
+    let mut buf = CONSOLE_BUFFER.lock();
+    buf.drain_buffer();
 }
